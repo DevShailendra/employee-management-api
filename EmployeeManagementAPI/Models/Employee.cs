f64253e5398c3313546d3c205874eb900b52fd35
Shailendra Dwivedi <luckydwivedi2001@gmail.com>
2026-03-12 00:40:35 +0530

    Initial ASP.NET Core Web API project setup

---
+++ EmployeeManagementAPI/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EmployeeManagementAPI.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
